95fd4f2e054157cbed5c572ff35ca71901ba7463
holovkoWork <holovko.work@gmail.com>
2017-07-02 14:43:54 +0200

    SwitFarbLandingLast

--- SwitFarb/gulpfile.js
+++ SwitFarb/gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('headersass', function() {
 gulp.task('libs', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/owl2/dist/owl.carousel.min.js'
+        'app/libs/owl-carousel/owl.carousel.min.js',
 		])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
